fc2b22fc43234c968ff737614779ec6d30afeeb1
Hari Gollapalli <hgollapalli@snaproute.com>
2015-12-04 13:56:34 -0800

    Syslog support for RIBD

--- rib/ribmain.go
+++ rib/ribmain.go
@@ -1,28 +1,42 @@
-package main 
-import ("os"
-		  "log"
-        "git.apache.org/thrift.git/lib/go/thrift"
-        "ribd"
-        )
+package main
+
+import (
+	"flag"
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"log"
+	"log/syslog"
+	"os"
+	"ribd"
+)
 
 var logger *log.Logger
 
-func main () {
-    var transport thrift.TServerTransport
-    var err error
-	 var addr = "localhost:5000"
+func main() {
+	var transport thrift.TServerTransport
+	var err error
+	var addr = "localhost:5000"
 
-    logger = log.New(os.Stdout, "RIBD :", log.Ldate|log.Ltime|log.Lshortfile)                                
+	logger = log.New(os.Stdout, "RIBD :", log.Ldate|log.Ltime|log.Lshortfile)
 
-    transport, err = thrift.NewTServerSocket(addr)
-	 if err != nil {
-		  logger.Println("Failed to create Socket with:", addr)
-	 }
-    handler := NewRouteServiceHandler()
-    processor := ribd.NewRouteServiceProcessor(handler)
-    transportFactory := thrift.NewTBufferedTransportFactory(8192) 
-    protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
-    server := thrift.NewTSimpleServer4(processor, transport, transportFactory, protocolFactory)
-    logger.Println("Starting RIB daemon")
-    server.Serve()
+	syslogger, err := syslog.New(syslog.LOG_NOTICE|syslog.LOG_INFO|syslog.LOG_DAEMON, "RIBD")
+	if err == nil {
+		syslogger.Info("### RIB Daemon started")
+		logger.SetOutput(syslogger)
+	}
+
+	paramsDir := flag.String("params", "", "Directory Location for config files")
+	logger.Println("### Params Dir ", paramsDir)
+	flag.Parse()
+
+	transport, err = thrift.NewTServerSocket(addr)
+	if err != nil {
+		logger.Println("Failed to create Socket with:", addr)
+	}
+	handler := NewRouteServiceHandler()
+	processor := ribd.NewRouteServiceProcessor(handler)
+	transportFactory := thrift.NewTBufferedTransportFactory(8192)
+	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+	server := thrift.NewTSimpleServer4(processor, transport, transportFactory, protocolFactory)
+	logger.Println("Starting RIB daemon")
+	server.Serve()
 }
