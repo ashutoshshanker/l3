e2cc351b0591ef66f03e7ce78591f51a5a87bd41
ukoranne <ukoranne@github.com>
2016-01-29 19:21:50 -0800

    OSPF  FSM dbd packet rx tx

--- ospf/server/ospfDD.go
+++ ospf/server/ospfDD.go
@@ -2,6 +2,10 @@ package server
 
 import (
 	"encoding/binary"
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"net"
 )
 
 /*
@@ -40,7 +44,7 @@ This file decodes database description packets.as per below format
 /* TODO
 remote hardcoding and get it while config.
 */
-const INTF_MTU_MIN = 1600
+const INTF_MTU_MIN = 1500
 
 type ospfDatabaseDescriptionData struct {
 	options            uint8
@@ -64,18 +68,29 @@ func decodeDatabaseDescriptionData(data []byte, dbd_data *ospfDatabaseDescriptio
 	dbd_data.mbit = imms_options&0x02 != 0
 	dbd_data.msbit = imms_options&0x01 != 0
 
-	/*fmt.Println("Decoded packet options:", dbd_data.options,
-	"IMMS:", dbd_data.ibit, dbd_data.mbit, dbd_data.msbit,
-	"seq num:", dbd_data.dd_sequence_number) */
+	fmt.Println("Decoded packet options:", dbd_data.options,
+		"IMMS:", dbd_data.ibit, dbd_data.mbit, dbd_data.msbit,
+		"seq num:", dbd_data.dd_sequence_number)
 }
 
 func encodeDatabaseDescriptionData(dd_data ospfDatabaseDescriptionData) []byte {
 	pkt := make([]byte, INTF_MTU_MIN)
 	binary.BigEndian.PutUint16(pkt[0:2], dd_data.interface_mtu)
 	pkt[2] = dd_data.options
+	imms := 0x07
+	if !dd_data.ibit {
+		imms = imms & 0x04
+	}
+	if !dd_data.mbit {
+		imms = imms & 0x02
+	}
+	if !dd_data.msbit {
+		imms = imms & 0x01
+	}
+	pkt[3] = byte(imms)
 	//	pkt[3] = dd_data.ibit | dd_data.mbit | dd_data.msbit
 	binary.BigEndian.PutUint32(pkt[4:8], dd_data.dd_sequence_number)
-	//fmt.Println("data consrtructed  ", pkt)
+	fmt.Println("data consrtructed  ", pkt)
 	return pkt
 }
 
@@ -84,13 +99,92 @@ func constructDatabaseDescriptionPaket(intf IntfConf, nbr OspfNeighborEntry) {
 
 }
 */
+func (server *OSPFServer) BuildAndSendDBDPkt(intfKey IntfConfKey, ent IntfConf, nbrConf OspfNeighborEntry, dbdData ospfDatabaseDescriptionData) {
+	ospfHdr := OSPFHeader{
+		ver:      OSPF_VERSION_2,
+		pktType:  uint8(DBDescriptionType),
+		pktlen:   0,
+		routerId: server.ospfGlobalConf.RouterId,
+		areaId:   ent.IfAreaId,
+		chksum:   0,
+		authType: ent.IfAuthType,
+		//authKey:        ent.IfAuthKey,
+	}
+
+	ospfPktlen := OSPF_HEADER_SIZE
+	ospfPktlen = ospfPktlen + OSPF_DBD_MIN_SIZE
+
+	ospfHdr.pktlen = uint16(ospfPktlen)
+
+	ospfEncHdr := encodeOspfHdr(ospfHdr)
+	server.logger.Info(fmt.Sprintln("ospfEncHdr:", ospfEncHdr))
+	dbdDataEnc := encodeDatabaseDescriptionData(dbdData)
+	server.logger.Info(fmt.Sprintln("DBD Pkt:", dbdDataEnc))
+
+	ospf := append(ospfEncHdr, dbdDataEnc...)
+	server.logger.Info(fmt.Sprintln("OSPF DBD:", ospf))
+	csum := computeCheckSum(ospf)
+	binary.BigEndian.PutUint16(ospf[12:14], csum)
+	copy(ospf[16:24], ent.IfAuthKey)
+
+	ipPktlen := IP_HEADER_MIN_LEN + ospfHdr.pktlen
+	ipLayer := layers.IPv4{
+		Version:  uint8(4),
+		IHL:      uint8(IP_HEADER_MIN_LEN),
+		TOS:      uint8(0xc0),
+		Length:   uint16(ipPktlen),
+		TTL:      uint8(1),
+		Protocol: layers.IPProtocol(OSPF_PROTO_ID),
+		SrcIP:    ent.IfIpAddr,
+		DstIP:    net.IP{224, 0, 0, 5},
+	}
+
+	ethLayer := layers.Ethernet{
+		SrcMAC:       ent.IfMacAddr,
+		DstMAC:       net.HardwareAddr{0x01, 0x00, 0x5e, 0x00, 0x00, 0x05},
+		EthernetType: layers.EthernetTypeIPv4,
+	}
+
+	buffer := gopacket.NewSerializeBuffer()
+	options := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	gopacket.SerializeLayers(buffer, options, &ethLayer, &ipLayer, gopacket.Payload(ospf))
+	server.logger.Info(fmt.Sprintln("buffer: ", buffer))
+	dbdPkt := buffer.Bytes()
+	server.logger.Info(fmt.Sprintln("dbdPkt: ", dbdPkt))
+	/*
+		TODO get retransmit value from INtfconf
+	*/
+
+	go server.SendDBDPkt(intfKey, dbdPkt)
+	//return ospfPkt
+}
+
+func (server *OSPFServer) SendDBDPkt(intfKey IntfConfKey, data []byte) {
+	server.SendOspfPkt(intfKey, data)
+
+}
 
 func (server *OSPFServer) processRxDbdPkt(data []byte, ospfHdrMd *OspfHdrMetadata, ipHdrMd *IpHdrMetadata, key IntfConfKey) error {
 	//ent, _ := server.IntfConfMap[key]
 	ospfdbd_data := newOspfDatabaseDescriptionData()
+	routerId := convertIPv4ToUint32(ospfHdrMd.routerId)
 	/*  TODO check min length
 	 */
 	decodeDatabaseDescriptionData(data, ospfdbd_data)
-
+	dbdNbrMsg := ospfNeighborDBDMsg{
+		ospfNbrConfKey: NeighborConfKey{
+			OspfNbrRtrId: routerId,
+		},
+		ospfNbrDBDData: *ospfdbd_data,
+	}
+	server.neighborDBDEventCh <- dbdNbrMsg
 	return nil
 }
+
+/*
+func (server *OSPFServer) sendDBDPkt(dbd_pkt ospfDatabaseDescriptionData, nbrKey NeighborConfKey, intfKey IntfConfKey) {
+
+}*/
