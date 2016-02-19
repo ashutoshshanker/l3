b07cd4959705636162fce2b30ab6941ff1b6dd76
Avoy Nanda <ananda@snaproute.com>
2016-02-19 11:39:13 -0800

    Fixes

--- bfd/rpc/listener_update.go
+++ bfd/rpc/listener_update.go
@@ -15,8 +15,12 @@ func (h *BFDHandler) UpdateBfdGlobalConfig(origConf *bfdd.BfdGlobalConfig, newCo
 
 func (h *BFDHandler) UpdateBfdIntfConfig(origConf *bfdd.BfdIntfConfig, newConf *bfdd.BfdIntfConfig, attrset []bool) (bool, error) {
 	h.logger.Info(fmt.Sprintln("Original interface config attrs:", origConf))
-	h.logger.Info(fmt.Sprintln("New interface config attrs:", newConf))
-	return true, nil
+	if newConf == nil {
+		err := errors.New("Invalid Interface Configuration")
+		return false, err
+	}
+	h.logger.Info(fmt.Sprintln("Updated interface config attrs:", newConf))
+	return h.SendBfdIntfConfig(newConf), nil
 }
 
 func (h *BFDHandler) UpdateBfdSessionConfig(origConf *bfdd.BfdSessionConfig, newConf *bfdd.BfdSessionConfig, attrset []bool) (bool, error) {
