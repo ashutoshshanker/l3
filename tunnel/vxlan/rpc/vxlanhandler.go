fa1b2ddb600095378291db0e4be9cfb08157a88e
ccordes-snaproute <ccordes@snaproute.com>
2016-04-04 11:44:46 -0700

    removed vxlansupport

--- tunnel/vxlan/rpc/vxlanhandler.go
+++ tunnel/vxlan/rpc/vxlanhandler.go
@@ -14,8 +14,6 @@ import (
 
 const DBName string = "UsrConfDb.db"
 
-var VxlanSupported bool = false
-
 type VXLANDServiceHandler struct {
 	server *vxlan.VXLANServer
 	logger *logging.Writer
@@ -62,10 +60,6 @@ func (v *VXLANDServiceHandler) StartThriftServer() {
 }
 
 func (v *VXLANDServiceHandler) CreateVxlanInstance(config *vxland.VxlanInstance) (bool, error) {
-	if !VxlanSupported {
-		return false, fmt.Errorf("VXLAN: not supported")
-	}
-
 	v.logger.Info(fmt.Sprintf("CreateVxlanConfigInstance %#v", config))
 
 	c, err := v.server.ConvertVxlanInstanceToVxlanConfig(config)
@@ -103,10 +97,6 @@ func (v *VXLANDServiceHandler) UpdateVxlanInstance(origconfig *vxland.VxlanInsta
 }
 
 func (v *VXLANDServiceHandler) CreateVxlanVtepInstances(config *vxland.VxlanVtepInstances) (bool, error) {
-	if !VxlanSupported {
-		return false, fmt.Errorf("VXLAN: not supported")
-	}
-
 	v.logger.Info(fmt.Sprintf("CreateVxlanVtepInstances %#v", config))
 	c, err := v.server.ConvertVxlanVtepInstanceToVtepConfig(config)
 	if err == nil {
