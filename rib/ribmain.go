606fd971ef139004a1f3da7d65b01aac696a9d5a
mjoshi <global>
2016-04-19 10:26:32 -0700

    modified:   ribdUtils.go 	modified:   ribmain.go

--- rib/ribmain.go
+++ rib/ribmain.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"git.apache.org/thrift.git/lib/go/thrift"
 	"ribd"
+	"strconv"
 	"utils/logging"
 	"utils/policy"
 	"database/sql"
+	"io/ioutil"
+	"encoding/json"
 )
 
 var logger *logging.Writer
@@ -15,11 +18,29 @@ var routeServiceHandler *RIBDServicesHandler
 var PARAMSDIR string
 var PolicyEngineDB *policy.PolicyEngineDB
 
+func getClient(logger *logging.Writer, fileName string, process string) (*ClientJson, error) {
+	var allClients []ClientJson
+
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		logger.Err(fmt.Sprintf("Failed to open RIBd config file:%s, err:%s", fileName, err))
+		return nil, err
+	}
+
+	json.Unmarshal(data, &allClients)
+	for _, client := range allClients {
+		if client.Name == process {
+			return &client, nil
+		}
+	}
+
+	logger.Err(fmt.Sprintf("Did not find port for %s in config file:%s", process, fileName))
+	return nil, nil
+}
+
 func main() {
 	var transport thrift.TServerTransport
 	var err error
-	var addr = "localhost:5000"
-	fmt.Println("Starting rib daemon")
 	paramsDir := flag.String("params", "./params", "Params directory")
 	flag.Parse()
 	fileName := *paramsDir
@@ -47,6 +68,13 @@ func main() {
 		fmt.Println(fmt.Sprintln("Failed to keep DB connection alive"))
 		return 
 	}
+	clientJson, err := getClient(logger, fileName+"clients.json", "ribd")
+	if err != nil || clientJson == nil {
+		return
+	}
+	var addr = "localhost:" + strconv.Itoa(clientJson.Port)//"localhost:5000"
+	fmt.Println("Starting rib daemon at addr ", addr)
+
 	transport, err = thrift.NewTServerSocket(addr)
 	if err != nil {
 		logger.Info(fmt.Sprintln("Failed to create Socket with:", addr))
