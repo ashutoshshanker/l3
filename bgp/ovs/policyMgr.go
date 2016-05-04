7aec30a579f5f40f5ca092bd1e12f350560b118f
Jay Gheewala <jgheewala@snaproute.com>
2016-05-04 06:37:40 -0700

    route entry is obtained, however nexthop entry is not

--- bgp/ovs/policyMgr.go
+++ bgp/ovs/policyMgr.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"l3/bgp/utils"
 	"sync"
+
+	"github.com/socketplane/libovsdb"
 )
 
 const (
@@ -37,30 +39,67 @@ func (mgr *OvsPolicyMgr) Start() {
 	go mgr.handleRedistribute()
 }
 
+/*
+func (mgr *FSRouteMgr) populateConfigRoute(route *ribdInt.Routes) *config.RouteInfo {
+	rv := &config.RouteInfo{
+		Ipaddr:           route.Ipaddr,
+		Mask:             route.Mask,
+		NextHopIp:        route.NextHopIp,
+		Prototype:        int(route.Prototype),
+		NetworkStatement: route.NetworkStatement,
+		RouteOrigin:      route.RouteOrigin,
+	}
+	return rv
+}
+*/
+func (mgr *OvsPolicyMgr) sendConnectedRoutes(add bool) {
+	routeEntries, exists :=
+		mgr.dbmgr.cache[ROUTE_TABLE]
+	if !exists {
+		return
+	}
+	for key, value := range routeEntries {
+		if value.Fields["from"] == "connected" {
+			utils.Logger.Info(fmt.Sprintln("Key:", key))
+			utils.Logger.Info(fmt.Sprintln("Value:", value))
+			nhId, ok := value.Fields["nexthops"].(libovsdb.UUID)
+			if !ok {
+				utils.Logger.Err(fmt.Sprintln("No next hop configured for",
+					value.Fields["prefix"]))
+				continue
+			}
+			utils.Logger.Info(fmt.Sprintln("nh:", nhId))
+			nhs, exists := mgr.dbmgr.cache["Nexthop"]
+			if !exists {
+				utils.Logger.Err(fmt.Sprintln("No next hop configured for",
+					value.Fields["prefix"]))
+				continue
+			}
+
+			nh, exists := nhs[string(mgr.dbmgr.getObjUUID(nhId))]
+			if !exists {
+				utils.Logger.Err(fmt.Sprintln("No next hop configured for",
+					value.Fields["prefix"]))
+				continue
+			}
+			utils.Logger.Info(fmt.Sprintln("NextHop is", nh))
+		}
+	}
+}
+
 func (mgr *OvsPolicyMgr) handleRedistribute() {
 	for {
-		routeEntries, exists :=
-			mgr.dbmgr.cache[ROUTE_TABLE]
-		if !exists {
-			continue
-		}
+		//@TODO: we need to set some local variable to be true indicating that
+		// redistribute policy is set... so when a route is installed after bgp is configured
+		// then we need to send that route to bgp server
 		select {
 		case conn := <-mgr.connected:
-			if conn {
-				utils.Logger.Info(fmt.Sprintln("Send Connected Route Entries:",
-					routeEntries))
-			} else {
-				utils.Logger.Info("Remove connected Routes")
-			}
+			mgr.sendConnectedRoutes(conn)
 		case static := <-mgr.static:
 			if static {
-				utils.Logger.Info(fmt.Sprintln("Send Static Route Entries:",
-					routeEntries))
 			}
 		case ospf := <-mgr.ospf:
 			if ospf {
-				utils.Logger.Info(fmt.Sprintln("Send Ospf Route Entries:",
-					routeEntries))
 			}
 		}
 	}
